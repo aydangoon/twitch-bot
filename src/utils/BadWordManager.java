e9cb7f6cf23fb7ca9469c5e875e529e756fae310
hen-chen <henchen@seas.upenn.edu>
2021-05-06 01:55:40 -0400

    fixed censoring using .txt

--- src/utils/BadWordManager.java
+++ src/utils/BadWordManager.java
@@ -1,11 +1,31 @@
 package utils;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class BadWordManager {
-	private static final String[] badWords = {
-		"fuck", "shit", "retard", "ass", "bitch", "cock", "cum", "fag", "dick", "gay"
-	};
+    static Set<String> badWords = new HashSet<String>();
+    static boolean hasDone = false;
+    
+//	private static final String[] badWords = {
+//		"fuck", "shit", "retard", "ass", "bitch", "cock", "cum", "fag", "dick", "gay"
+//	};
 	
 	public static boolean shouldCensor(String word) {
+	    if (!hasDone) {
+	        try {
+	            File obj = new File("./data/badwords.txt");
+	            Scanner scan = new Scanner(obj);
+	            while (scan.hasNextLine()) {
+	                badWords.add(scan.nextLine());
+	            }
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        }
+	        hasDone = true;
+	    }
 		for (String badWord : badWords) {
 			if (word.toLowerCase().contains(badWord)) {
 				return true;
